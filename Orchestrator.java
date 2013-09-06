4de97ef2ead75f1a5dcb3461f5ecc6cc5b50a4e0
abhinavdangeti <abhinav@couchbase.com>
2013-09-06 11:39:24 -0700

    Providing a loop option for the load

--- Orchestrator.java
+++ Orchestrator.java
@@ -28,6 +28,7 @@ public class Orchestrator {
     private static double _expRatio = 0.0;
     private static int _expiration = 600;		//10 minutes default
     private static int _addCount = 0;
+    private static boolean _loop = false;       //to continuously loop the load
 
     public static void main(String args[]) throws InterruptedException {
 
@@ -47,88 +48,97 @@ public class Orchestrator {
                 _appendRatio, _appendSize, _prependRatio, _prependSize,
                 _delRatio, _expRatio, _expiration, _addCount);
 
-        for (int i=0; i<_buckets.length; i++) {
-            String bucketname = _buckets[i].split(":")[0];
-            String bucketpasswd = "";
-            if (_buckets[i].contains(":"))
-                bucketpasswd = _buckets[i].split(":")[1];
+        int _x_ = 0;
 
-            final CouchbaseClient client = connect(_Nodes[0], bucketname, bucketpasswd);
+        while (_x_ < 1) {
+            for (int i=0; i<_buckets.length; i++) {
+                String bucketname = _buckets[i].split(":")[0];
+                String bucketpasswd = "";
+                if (_buckets[i].contains(":"))
+                    bucketpasswd = _buckets[i].split(":")[1];
 
-            Runnable _sets_ = new Runnable() {
-                public void run() {
-                    // System.out.println("Sets thread starts");
-                    try {
-                        Sets.set_items(client, V, _prefix);
-                    } catch (Exception e) {
-                        // e.printStackTrace();
+                final CouchbaseClient client = connect(_Nodes[0], bucketname, bucketpasswd);
+
+                Runnable _sets_ = new Runnable() {
+                    public void run() {
+                        // System.out.println("Sets thread starts");
+                        try {
+                            Sets.set_items(client, V, _prefix);
+                        } catch (Exception e) {
+                            // e.printStackTrace();
+                        }
                     }
-                }
-            };
+                };
 
-            Runnable _gets_ = new Runnable() {
-                public void run() {
-                    // System.out.println("Gets thread starts");
-                    try {
-                        Gets.get_items(client, V, _prefix);
-                    } catch (Exception e) {
-                        // e.printStackTrace();
+                Runnable _gets_ = new Runnable() {
+                    public void run() {
+                        // System.out.println("Gets thread starts");
+                        try {
+                            Gets.get_items(client, V, _prefix);
+                        } catch (Exception e) {
+                            // e.printStackTrace();
+                        }
                     }
-                }
-            };
+                };
 
-            Runnable _mods_ = new Runnable() {
-                public void run() {
-                    // System.out.println("Mods thread starts");
-                    try {
-                        Mods.mod_items(client, V, _prefix);
-                    } catch (Exception e) {
-                        // e.printStackTrace();
+                Runnable _mods_ = new Runnable() {
+                    public void run() {
+                        // System.out.println("Mods thread starts");
+                        try {
+                            Mods.mod_items(client, V, _prefix);
+                        } catch (Exception e) {
+                            // e.printStackTrace();
+                        }
                     }
-                }
-            };
+                };
 
-            Runnable _adds_ = new Runnable() {
-                public void run() {
-                    // System.out.println("Adds thread starts");
-                    try {
-                        Adds.add_items(client, V, _prefix);
-                    } catch (Exception e) {
-                        // e.printStackTrace();
+                Runnable _adds_ = new Runnable() {
+                    public void run() {
+                        // System.out.println("Adds thread starts");
+                        try {
+                            Adds.add_items(client, V, _prefix);
+                        } catch (Exception e) {
+                            // e.printStackTrace();
+                        }
                     }
-                }
-            };
+                };
 
-            Runnable _dels_ = new Runnable() {
-                public void run() {
-                    // System.out.println("Deletes thread starts");
-                    try {
-                        Deletes.del_items(client, V, _prefix);
-                    } catch (Exception e) {
-                        // e.printStackTrace();
+                Runnable _dels_ = new Runnable() {
+                    public void run() {
+                        // System.out.println("Deletes thread starts");
+                        try {
+                            Deletes.del_items(client, V, _prefix);
+                        } catch (Exception e) {
+                            // e.printStackTrace();
+                        }
                     }
-                }
-            };
+                };
 
-            Thread _sets = new Thread(_sets_);
-            Thread _gets = new Thread(_gets_);
-            Thread _mods = new Thread(_mods_);
-            Thread _adds = new Thread(_adds_);
-            Thread _dels = new Thread(_dels_);
+                Thread _sets = new Thread(_sets_);
+                Thread _gets = new Thread(_gets_);
+                Thread _mods = new Thread(_mods_);
+                Thread _adds = new Thread(_adds_);
+                Thread _dels = new Thread(_dels_);
 
-            _sets.start();      //Begin sets thread
-            _gets.start();      //Begin gets thread
-            _sets.join();       //Wait for sets thread to complete
-            _dels.start();      //Begin deletes thread
-            _mods.start();      //Begin mods thread
-            _adds.start();      //Begin adds thread
-            _dels.join();       //Wait for deletes thread to complete
-            _adds.join();       //Wait for adds thread to complete
-            _mods.join();       //Wait for mods thread to complete
-            _gets.join();       //Wait for gets thread to complete
+                _sets.start();      //Begin sets thread
+                _gets.start();      //Begin gets thread
+                _sets.join();       //Wait for sets thread to complete
+                _dels.start();      //Begin deletes thread
+                _mods.start();      //Begin mods thread
+                _adds.start();      //Begin adds thread
+                _dels.join();       //Wait for deletes thread to complete
+                _adds.join();       //Wait for adds thread to complete
+                _mods.join();       //Wait for mods thread to complete
+                _gets.join();       //Wait for gets thread to complete
+                client.shutdown();
 
-            client.shutdown();
+            }
+
+            if (!_loop)
+                _x_++;
+
         }
+
         System.out.println(" ........... done ...........");
         System.exit(0);
 
@@ -191,6 +201,8 @@ public class Orchestrator {
                 _expiration = (Integer.parseInt(properties.getProperty(key)));
             if (key.equals("add-count"))
                 _addCount = (Integer.parseInt(properties.getProperty(key)));
+            if (key.equals("loop"))
+                _loop = (Boolean.parseBoolean(properties.getProperty(key)));
         }
     }
 }
