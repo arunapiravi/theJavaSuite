58594bdb866774204aabd6cc310ef79bb5b44f2b
abhinavdangeti <abhinav@couchbase.com>
2013-10-22 16:23:40 -0700

    Adding Synchronous access over multiple buckets

--- Orchestrator.java
+++ Orchestrator.java
@@ -65,7 +65,7 @@ public class Orchestrator {
                     public void run() {
                         // System.out.println("Client thread starts");
                         try {
-                            runClientOperations(client, V);
+                            OperationsHub.runClientOperations(client, V, _prefix);
                         } catch (Exception e) {
                             // e.printStackTrace();
                         }
@@ -91,83 +91,7 @@ public class Orchestrator {
 
     }
 
-    private static void runClientOperations(final CouchbaseClient client, final Variables V)
-                                    throws InterruptedException {
 
-        Runnable _sets_ = new Runnable() {
-            public void run() {
-                // System.out.println("Sets thread starts");
-                try {
-                    Sets.set_items(client, V, _prefix);
-                } catch (Exception e) {
-                    // e.printStackTrace();
-                }
-            }
-        };
-
-        Runnable _gets_ = new Runnable() {
-            public void run() {
-                // System.out.println("Gets thread starts");
-                try {
-                    Gets.get_items(client, V, _prefix);
-                } catch (Exception e) {
-                    // e.printStackTrace();
-                }
-            }
-        };
-
-        Runnable _mods_ = new Runnable() {
-            public void run() {
-                // System.out.println("Mods thread starts");
-                try {
-                    Mods.mod_items(client, V, _prefix);
-                } catch (Exception e) {
-                    // e.printStackTrace();
-                }
-            }
-        };
-
-        Runnable _adds_ = new Runnable() {
-            public void run() {
-                // System.out.println("Adds thread starts");
-                try {
-                    Adds.add_items(client, V, _prefix);
-                } catch (Exception e) {
-                    // e.printStackTrace();
-                }
-            }
-        };
-
-        Runnable _dels_ = new Runnable() {
-            public void run() {
-                // System.out.println("Deletes thread starts");
-                try {
-                    Deletes.del_items(client, V, _prefix);
-                } catch (Exception e) {
-                    // e.printStackTrace();
-                }
-            }
-        };
-
-        Thread _sets = new Thread(_sets_);
-        Thread _gets = new Thread(_gets_);
-        Thread _mods = new Thread(_mods_);
-        Thread _adds = new Thread(_adds_);
-        Thread _dels = new Thread(_dels_);
-
-        _sets.start();      //Begin sets thread
-        _gets.start();      //Begin gets thread
-        _sets.join();       //Wait for sets thread to complete
-        _dels.start();      //Begin deletes thread
-        _mods.start();      //Begin mods thread
-        _adds.start();      //Begin adds thread
-        _dels.join();       //Wait for deletes thread to complete
-        _adds.join();       //Wait for adds thread to complete
-        _mods.join();       //Wait for mods thread to complete
-        _gets.join();       //Wait for gets thread to complete
-        client.shutdown();
-
-    }
 
     private static CouchbaseClient connect(String _addr, String _bucketName, String _bucketPasswd) {
         /*
