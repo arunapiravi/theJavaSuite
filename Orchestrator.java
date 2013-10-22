f1de65faf4bb991cdef8eadee3ea9ec23d2fe744
abhinavdangeti <abhinav@couchbase.com>
2013-10-22 15:55:37 -0700

    Adding Synchronous access over multiple buckets

--- Orchestrator.java
+++ Orchestrator.java
@@ -51,6 +51,8 @@ public class Orchestrator {
         int _x_ = 0;
 
         while (_x_ < 1) {
+
+            Thread[] _control = new Thread[_buckets.length];
             for (int i=0; i<_buckets.length; i++) {
                 String bucketname = _buckets[i].split(":")[0];
                 String bucketpasswd = "";
@@ -59,79 +61,24 @@ public class Orchestrator {
 
                 final CouchbaseClient client = connect(_Nodes[0], bucketname, bucketpasswd);
 
-                Runnable _sets_ = new Runnable() {
+                Runnable _control_ = new Runnable() {
                     public void run() {
-                        // System.out.println("Sets thread starts");
+                        // System.out.println("Client thread starts");
                         try {
-                            Sets.set_items(client, V, _prefix);
+                            runClientOperations(client, V);
                         } catch (Exception e) {
                             // e.printStackTrace();
                         }
                     }
                 };
 
-                Runnable _gets_ = new Runnable() {
-                    public void run() {
-                        // System.out.println("Gets thread starts");
-                        try {
-                            Gets.get_items(client, V, _prefix);
-                        } catch (Exception e) {
-                            // e.printStackTrace();
-                        }
-                    }
-                };
+                _control[i] = new Thread(_control_);
+                _control[i].start();
 
-                Runnable _mods_ = new Runnable() {
-                    public void run() {
-                        // System.out.println("Mods thread starts");
-                        try {
-                            Mods.mod_items(client, V, _prefix);
-                        } catch (Exception e) {
-                            // e.printStackTrace();
-                        }
-                    }
-                };
+            }
 
-                Runnable _adds_ = new Runnable() {
-                    public void run() {
-                        // System.out.println("Adds thread starts");
-                        try {
-                            Adds.add_items(client, V, _prefix);
-                        } catch (Exception e) {
-                            // e.printStackTrace();
-                        }
-                    }
-                };
-
-                Runnable _dels_ = new Runnable() {
-                    public void run() {
-                        // System.out.println("Deletes thread starts");
-                        try {
-                            Deletes.del_items(client, V, _prefix);
-                        } catch (Exception e) {
-                            // e.printStackTrace();
-                        }
-                    }
-                };
-
-                Thread _sets = new Thread(_sets_);
-                Thread _gets = new Thread(_gets_);
-                Thread _mods = new Thread(_mods_);
-                Thread _adds = new Thread(_adds_);
-                Thread _dels = new Thread(_dels_);
-
-                _sets.start();      //Begin sets thread
-                _gets.start();      //Begin gets thread
-                _sets.join();       //Wait for sets thread to complete
-                _dels.start();      //Begin deletes thread
-                _mods.start();      //Begin mods thread
-                _adds.start();      //Begin adds thread
-                _dels.join();       //Wait for deletes thread to complete
-                _adds.join();       //Wait for adds thread to complete
-                _mods.join();       //Wait for mods thread to complete
-                _gets.join();       //Wait for gets thread to complete
-                client.shutdown();
-
+            for (int j=0; j<_buckets.length; j++) {
+                _control[j].join();
             }
 
             if (!_loop)
@@ -141,6 +88,84 @@ public class Orchestrator {
 
         System.out.println(" ........... done ...........");
         System.exit(0);
+
+    }
+
+    private static void runClientOperations(final CouchbaseClient client, final Variables V)
+                                    throws InterruptedException {
+
+        Runnable _sets_ = new Runnable() {
+            public void run() {
+                // System.out.println("Sets thread starts");
+                try {
+                    Sets.set_items(client, V, _prefix);
+                } catch (Exception e) {
+                    // e.printStackTrace();
+                }
+            }
+        };
+
+        Runnable _gets_ = new Runnable() {
+            public void run() {
+                // System.out.println("Gets thread starts");
+                try {
+                    Gets.get_items(client, V, _prefix);
+                } catch (Exception e) {
+                    // e.printStackTrace();
+                }
+            }
+        };
+
+        Runnable _mods_ = new Runnable() {
+            public void run() {
+                // System.out.println("Mods thread starts");
+                try {
+                    Mods.mod_items(client, V, _prefix);
+                } catch (Exception e) {
+                    // e.printStackTrace();
+                }
+            }
+        };
+
+        Runnable _adds_ = new Runnable() {
+            public void run() {
+                // System.out.println("Adds thread starts");
+                try {
+                    Adds.add_items(client, V, _prefix);
+                } catch (Exception e) {
+                    // e.printStackTrace();
+                }
+            }
+        };
+
+        Runnable _dels_ = new Runnable() {
+            public void run() {
+                // System.out.println("Deletes thread starts");
+                try {
+                    Deletes.del_items(client, V, _prefix);
+                } catch (Exception e) {
+                    // e.printStackTrace();
+                }
+            }
+        };
+
+        Thread _sets = new Thread(_sets_);
+        Thread _gets = new Thread(_gets_);
+        Thread _mods = new Thread(_mods_);
+        Thread _adds = new Thread(_adds_);
+        Thread _dels = new Thread(_dels_);
+
+        _sets.start();      //Begin sets thread
+        _gets.start();      //Begin gets thread
+        _sets.join();       //Wait for sets thread to complete
+        _dels.start();      //Begin deletes thread
+        _mods.start();      //Begin mods thread
+        _adds.start();      //Begin adds thread
+        _dels.join();       //Wait for deletes thread to complete
+        _adds.join();       //Wait for adds thread to complete
+        _mods.join();       //Wait for mods thread to complete
+        _gets.join();       //Wait for gets thread to complete
+        client.shutdown();
 
     }
 
